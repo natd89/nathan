1c5efa997ce406a7cc4fe91d035492bd3b6d288f
nmd89 <nmd89@byu.edu>
2017-12-08 15:38:28 -0700

    my emacs file

---
+++ .emacs.d/elpa/deferred-20170901.630/deferred-autoloads.el
@@ -0,0 +1,15 @@
+;;; deferred-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("deferred.el") (23070 62815 431120 446000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; deferred-autoloads.el ends here
